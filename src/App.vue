39ee4a097a7c8982af70dddbd232a6e5dd2cc988
zhenquansim <s.zhenquan98@gmail.com>
2025-08-13 00:05:13 +0800

    Installed vue and added basic layout

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h1>FeedMe Kitchen Display System</h1>
+
+  <div class="columns-2">
+    <div class="bg-white rounded-xl">Pending</div>
+    <div class="bg-white rounded-xl">Completed</div>
+  </div>
+</template>
+
+<style scoped></style>
